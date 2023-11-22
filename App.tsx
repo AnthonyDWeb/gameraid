6d94cbbe1a9b571b14cf433f3dc0fef2a8778571
AnthonyDWeb <delforge.anthony@gmail.com>
2023-11-22 16:36:41 +0100

    add navigation folder and files + type

--- App.tsx
+++ App.tsx
@@ -1,20 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { RootStackParamList } from "./utils/types/types";
+import HomeScreen from "./views/HomeScreen";
+import DetailsScreen from "./views/DetailsScreen";
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+const Navigator = Stack.Navigator;
+const Screen = Stack.Screen;
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Gamer Aid App !</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<NavigationContainer>
+			<Navigator>
+				<Screen name="Home" component={HomeScreen} />
+				<Screen name="Details" component={DetailsScreen} options={({ route }) => ({ title: route.params.name })} />
+			</Navigator>
+		</NavigationContainer>
+	);
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const styles = StyleSheet.create({});
---
+++ utils/types/types.tsx
@@ -0,0 +1,24 @@
+import type { NativeStackScreenProps } from "@react-navigation/native-stack";
+
+export type RootStackParamList = {
+	Home: undefined;
+	Details: {
+    name?: string,
+		itemId: number;
+		otherParam: string;
+	};
+	homepage: undefined;
+	fightcalculator: undefined;
+	randomfight: undefined;
+};
+
+export type HomeProps = NativeStackScreenProps<RootStackParamList, "Home">;
+export type DetailsProps = NativeStackScreenProps<RootStackParamList, "Details">;
+export type HomepageProps = NativeStackScreenProps<RootStackParamList, "homepage">;
+export type FightCalculatorProps = NativeStackScreenProps<RootStackParamList, "fightcalculator">;
+export type RandomFightProps = NativeStackScreenProps<RootStackParamList, "randomfight">;
+
+// type StackNavigation = NativeStackScreenProps<RootStackParamList>;
+// export type StackNavigationProps = {
+// 	navigation: StackNavigation;
+// };
